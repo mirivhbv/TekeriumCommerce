5a055a4078b8fe0910572b4055614607cac6f8a9
Miri <mirivhbv@gmail.com>
2018-11-21 01:39:33 +0400

    Adds module.json and ModuleInitializer.cs

---
+++ src/Modules/TekeriumCommerce.Module.ActivityLog/ModuleInitializer.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using TekeriumCommerce.Infrastructure.Modules;
+using TekeriumCommerce.Module.ActivityLog.Data;
+using TekeriumCommerce.Module.ActivityLog.Events;
+using TekeriumCommerce.Module.Core.Events;
+
+namespace TekeriumCommerce.Module.ActivityLog
+{
+    public class ModuleInitializer : IModuleInitializer
+    {
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IActivityTypeRepository, ActivityRepository>();
+            serviceCollection.AddTransient<INotificationHandler<EntityViewed>, EntityViewedHandler>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+        }
+    }
+}
