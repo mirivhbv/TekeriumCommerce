738982fe0bb4afa68059de772db1fdead39504d2
Miri <mirivhbv@gmail.com>
2018-11-21 01:05:58 +0400

    Adds models name of Activity, ActivityType and MostViewEntityDto

---
+++ src/Modules/TekeriumCommerce.Module.ActivityLog/Models/Activity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TekeriumCommerce.Module.ActivityLog.Models
+{
+    public class Activity
+    {
+        public long ActivityTypeId { get; set; }
+
+        public ActivityType ActivityType { get; set; }
+
+        public long UserId { get; set; }
+
+        public DateTimeOffset CreatedOn { get; set; }
+
+        public long EntityId { get; set; }
+
+        [Required]
+        [StringLength(450)]
+        public string EntityTypeId { get; set; }
+    }
+}
