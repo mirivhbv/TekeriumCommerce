dcb76e00777e5d4890da8e0d45e90e33096bf38e
Miri <mirivhbv@gmail.com>
2018-12-13 17:31:41 +0400

    creates initial little search form component

---
+++ src/Modules/TekeriumCommerce.Module.Search/Areas/Search/Components/LittleSearchFormViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TekeriumCommerce.Infrastructure.Data;
+using TekeriumCommerce.Infrastructure.Web;
+using TekeriumCommerce.Module.Catalog.Models;
+using TekeriumCommerce.Module.Search.Areas.Search.ViewModels;
+
+namespace TekeriumCommerce.Module.Search.Areas.Search.Components
+{
+    public class LittleSearchFormViewComponent : ViewComponent
+    {
+        private readonly IRepository<TyreWidth> _widthRepository;
+
+        public LittleSearchFormViewComponent(IRepository<TyreWidth> widthRepository)
+        {
+            _widthRepository = widthRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = new LittleSearchForm
+            {
+                AvailableWidths = _widthRepository.Query()
+                    .Select(x => new SelectListItem {Value = x.Size, Text = x.Size}).ToList()
+            };
+            return View(this.GetViewPath(), model);
+        }
+    }
+}
---
+++ src/Modules/TekeriumCommerce.Module.Search/Areas/Search/ViewModels/LittleSearchForm.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TekeriumCommerce.Module.Search.Areas.Search.ViewModels
+{
+    public class LittleSearchForm
+    {
+        public IList<SelectListItem> AvailableWidths { get; set; } = new List<SelectListItem>();
+    }
+}
