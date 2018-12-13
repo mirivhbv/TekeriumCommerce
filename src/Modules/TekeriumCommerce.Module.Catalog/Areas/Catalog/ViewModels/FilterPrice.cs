c40f27006b5cce15855bacf91f21a0d942f8deab
Miri <mirivhbv@gmail.com>
2018-12-13 17:00:04 +0400

    creates filter options

---
+++ src/Modules/TekeriumCommerce.Module.Catalog/Areas/Catalog/ViewModels/FilterPrice.cs
@@ -0,0 +1,9 @@
+﻿namespace TekeriumCommerce.Module.Catalog.Areas.Catalog.ViewModels
+{
+    public class FilterPrice
+    {
+        public decimal MaxPrice { get; set; }
+
+        public decimal MinPrice { get; set; }
+    }
+}
