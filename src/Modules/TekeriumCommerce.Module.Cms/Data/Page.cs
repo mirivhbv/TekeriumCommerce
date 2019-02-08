27411a2644c50136ca0766bbd377cc58f455f35b
Miri <mirivhbv@gmail.com>
2019-02-09 02:42:40 +0400

    going to product localization

---
+++ src/Modules/TekeriumCommerce.Module.Cms/Data/Page.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TekeriumCommerce.Module.Core.Models;
+
+namespace TekeriumCommerce.Module.Cms.Data
+{
+    public class Page : Content
+    {
+        public string Body { get; set; }
+    }
+}
